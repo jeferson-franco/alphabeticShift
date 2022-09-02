74194cde9a48d5d2df87c59a94855436dd4632c9
jefersonfranco <jefersonfranco@protonmail.com>
2022-09-02 16:38:18 -0300

    update alphabeticShift.js

--- src/alphabeticShift.js
+++ src/alphabeticShift.js
@@ -1,7 +1,7 @@
 function solution(inputString) {
     return inputString
         .split('')
-        .map(char => char.charCodeAt(0) === 122 ? 'a' : String.fromCharCode(char.charCodeAt(0) + 1))
+        .map((char) => (char.charCodeAt(0) === 122 ? 'a' : String.fromCharCode(char.charCodeAt(0) + 1)))
         .join('');
 }
 
@@ -11,3 +11,20 @@ module.exports = solution;
 // npm i jest-cli -g
 // package.json: "scripts": { "test": "jest" }
 // npm test alphabeticShift
+
+// alternative solution
+// function solution(s) {
+//     let c = 'abcdefghijklmnopqrstuvwxyza';
+//     return s.replace(/./g, (x) => c[c.indexOf(x) + 1]);
+// }
+
+// alternative solution
+// B = Buffer;
+// solution = (s) => B([...B(s)].map((e) => ((e - 96) % 26) + 97)) + '';
+
+// alternative solution
+// const solution = (inputString) =>
+//     inputString
+//         .split('')
+//         .map((el) => (el === 'z' ? 'a' : String.fromCharCode(el.charCodeAt() + 1)))
+//         .join('');
